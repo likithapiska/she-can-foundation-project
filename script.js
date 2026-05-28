514731e9623bc29cacd71c9c2cdaba3b0a5d1b0e
Likitha Piska <likithapiska@gmail.com>
2026-05-28 21:39:40 +0530

    Frontend internship project

---
+++ script.js
@@ -0,0 +1,19 @@
+function showMessage(){
+
+  alert("Thank you for supporting She Can Foundation 🌸");
+
+}
+
+function scrollAbout(){
+
+  document.getElementById("about").scrollIntoView({
+    behavior:"smooth"
+  });
+
+}
+
+function toggleMode(){
+
+  document.body.classList.toggle("light");
+
+}
